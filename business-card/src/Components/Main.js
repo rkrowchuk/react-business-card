b20cf6e57a09209a80ac18541b8c1304df3f1ee8
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-05-24 13:45:14 -0700

    begin laying out main component

--- business-card/src/Components/Main.js
+++ business-card/src/Components/Main.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function Main() {
   return (
-    <div>Main</div>
+    <div className='main-container'>
+      <h1>Rhiannon Krowchuk</h1>
+      <h4>Full-Stack Developer</h4>
+      <p>github.com/rkrowchuk</p>
+      <button className="email-btn">Email</button>
+    </div>
   )
 }
