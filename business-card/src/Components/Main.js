35183506c48a49ebdb2faa42cc5e0775274b6ce0
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-05-24 13:56:28 -0700

    style about and interests in main component

--- business-card/src/Components/Main.js
+++ business-card/src/Components/Main.js
@@ -7,6 +7,10 @@ export default function Main() {
       <h4>Full-Stack Developer</h4>
       <p>github.com/rkrowchuk</p>
       <button className="email-btn">Email</button>
+      <h2>About</h2>
+      <p className='main-p'>I'm a creative-turned-developer with a passion for problem-solving and collaboration. With a background in designing live experiences that inspire behaviour change, I'm pivoting my focus to build products that create impact on a larger scale</p>
+      <h2>Interests</h2>
+      <p className='main-p'>I love travelling, trail running, board games and trying out new activities.</p>
     </div>
   )
 }
