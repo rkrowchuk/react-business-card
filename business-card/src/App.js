41c6bd7b7d32eabe9c5c435d0e5f5ce07dc48b4e
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-05-24 13:01:40 -0700

    create Main and Footer components

--- business-card/src/App.js
+++ business-card/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
+import Main from './Components/Main';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
